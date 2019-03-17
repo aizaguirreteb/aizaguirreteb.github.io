07c17931d23cfee4da1041f03f81733dd9a99bf0
Alejandra Izaguirre Tébar <aleizatebar@gmail.com>
2019-03-17 12:47:10 +0100

    Card  and animated intro

---
+++ js/intro.js
@@ -0,0 +1,16 @@
+//JS file to reveal intro for some seconds and the showing the actual page
+var timer = 0; //global variable
+
+$(document).ready( function () {
+
+        $("#site").hide(); 
+        timer = setInterval( showSite , 3000 ) 
+
+});
+
+function showSite() {
+      clearInterval(timer);
+
+      $("#intro").fadeOut();
+      $("#site").fadeIn();
+}
